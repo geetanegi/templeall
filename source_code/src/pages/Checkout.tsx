ad5dd2590c39d1e78bf3a3146e5668f79f8f1fcd
sgaurav <sgaurav@netlink.com>
2024-09-27 19:44:06 +0530

    api integrated of checkout

--- source_code/src/pages/Checkout.tsx
+++ source_code/src/pages/Checkout.tsx
@@ -9,6 +9,9 @@ import CheckoutCard from "../components/Checkout/Checkout";
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
 import BreadCumModal from "../components/Contests/Contest Components/BreadCumModal";
+import { API_URL } from "../services/enums";
+import apiService from "../services/apiService";
+import { ToastError } from "../components/Toast";
 
 const Checkout: React.FC = () => {
   const selectedCourseName = useSelector(
@@ -26,10 +29,66 @@ const Checkout: React.FC = () => {
   const selectedYardage = useSelector(
     (state: RootState) => state.courses.yardage,
   );
+  const selectedContestsList = useSelector(
+    (state: RootState) => state.courses.selectedContests,
+  );
+
+  const selectedContests =
+    selectedTeeType !== null &&
+    selectedContestsList[selectedTeeType]?.length > 0 &&
+    selectedContestsList[selectedTeeType];
+
+  const totalPrice =
+    selectedTeeType !== null &&
+    selectedContestsList[selectedTeeType]?.length > 0
+      ? selectedContestsList[selectedTeeType].reduce(
+          (acc, contest) => acc + contest.entryFee,
+          0,
+        )
+      : 0;
+
   const [selectedPaymentMethod, setSelectedPaymentMethod] = useState<
     "wallet" | "credit_card"
   >("wallet"); // Default to 'wallet'
   const [isModalOpen, setModalOpen] = useState<boolean>(false);
+
+  console.log("selectedContests", selectedContests);
+
+  const handleCheckoutCart = async () => {
+    const obj = {
+      data: {
+        playerId: 1,
+        registrationDate: "2024-09-11T13:05:51Z",
+        totalAmount: totalPrice,
+        cartInfo:
+          selectedContests && Array.isArray(selectedContests)
+            ? selectedContests.map((item) => ({
+                scheduleContestId: item.scheduleContestId,
+                amount: item.entryFee,
+              }))
+            : [],
+        payment: {
+          paymentMethod: "CARD",
+          paymentStatus: "SUCCESS",
+          transactionId: "@121222",
+        },
+      },
+    };
+
+    try {
+      const res = await apiService.post<any>(
+        API_URL.contestCheckoutCoreRegistrationSave,
+        obj,
+      );
+      if (res.status === 200 && !res.data.error) {
+        console.log(res.data.data);
+      } else if (res.data.error) {
+        ToastError(res.data.description || "Error fetching course data");
+      }
+    } catch (error) {
+      ToastError("Error fetching course data");
+    }
+  };
 
   return (
     <div className="grid min-h-screen w-full grid-cols-[65%_35%] overflow-x-hidden bg-[#ffffff] px-2">
@@ -114,7 +173,10 @@ const Checkout: React.FC = () => {
             </div>
           </div>
           {selectedPaymentMethod === "credit_card" && <CheckoutCard />}
-          <button className="relative mx-auto flex w-full items-center justify-center gap-1 rounded-md bg-[#95c11e] py-2 text-white">
+          <button
+            onClick={handleCheckoutCart}
+            className="relative mx-auto flex w-full items-center justify-center gap-1 rounded-md bg-[#95c11e] py-2 text-white"
+          >
             <ShoppingCart className="relative" />
             <span className="mx-2">Checkout</span>
           </button>

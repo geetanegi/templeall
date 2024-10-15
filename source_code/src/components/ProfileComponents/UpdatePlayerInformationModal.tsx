3566b2b092984675f3678857499298d891667de7
sgaurav <sgaurav@netlink.com>
2024-10-15 20:17:08 +0530

    added

--- source_code/src/components/ProfileComponents/UpdatePlayerInformationModal.tsx
+++ source_code/src/components/ProfileComponents/UpdatePlayerInformationModal.tsx
@@ -201,11 +201,6 @@ const UpdatePlayerInformationModal: React.FC<updateProfileModalprops> = ({
       return;
     }
   };
-  // const scrollbarStyles: React.CSSProperties = {
-  //   overflow: "auto", // Enable scrolling
-  //   scrollbarWidth: "none", // Firefox
-  //   msOverflowStyle: "none", // IE and Edge
-  // };
 
   return (
     <PageLoader isActive={loader}>

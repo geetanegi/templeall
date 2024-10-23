1ad244485285c22ed2ee1e804c7a2b897f65c81e
sgaurav <sgaurav@acecamgolf.com>
2024-10-23 10:12:24 +0000

    Merged PR 469: dev qr code
    dev qr code

---
+++ source_code/src/components/AdminPanel/courses/QRModal.tsx
@@ -0,0 +1,61 @@
+import React, { useRef } from "react";
+import QRCode from "react-qr-code";
+import Modal from "../../ModalComponent";
+
+interface QRModalProps {
+  openModal: boolean;
+  setOpenModal: (openModal: boolean) => void;
+  course: any;
+}
+
+const QRModal: React.FC<QRModalProps> = ({
+  openModal,
+  setOpenModal,
+  course,
+}) => {
+  const qrCodeRefs = useRef<{ [key: string]: SVGSVGElement | null }>({});
+  // Function to convert query params to an object
+  const getQueryParams = (): Record<string, string> => {
+    const queryString = course?.split("?")[1]; // Get the part after '?' in the URL
+    const params = new URLSearchParams(queryString);
+    const queryParamsObj: Record<string, string> = {};
+    for (let [key, value] of params.entries()) {
+      queryParamsObj[key] = value;
+    }
+    return queryParamsObj;
+  };
+
+  const queryParams = getQueryParams();
+  console.log("course", queryParams);
+  return (
+    <div>
+      <Modal isOpen={openModal} onClose={() => setOpenModal(false)} title={""}>
+        <div
+          className="mx-auto w-[500px] overflow-auto p-2 text-center"
+          style={{ maxHeight: "480px" }}
+        >
+          <div className="m-1 flex items-center justify-center">
+            <QRCode
+              key={course}
+              value={course}
+              size={256}
+              // level="H"
+              bgColor="#FFFFFF"
+              fgColor="#000000"
+              ref={(el: any) => (qrCodeRefs.current[course] = el)}
+            />
+          </div>
+          <div className="px-28 py-5 text-left">
+            <p>Course: {queryParams.courseName}</p>
+            <p>
+              {queryParams.holeNo && "Hole No: "}
+              {queryParams.holeNo}
+            </p>
+          </div>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+export default QRModal;

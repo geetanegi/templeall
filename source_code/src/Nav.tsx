7d03cd31ef963b0f06f5a7bf0ff0333954780599
sgaurav <sgaurav@acecamgolf.com>
2024-10-28 12:21:11 +0000

    Merged PR 530: Homepage UI sprint_5 pr
    Homepage UI sprint_5 pr  with mobile view removed earnings code

--- source_code/src/Nav.tsx
+++ source_code/src/Nav.tsx
@@ -258,30 +258,6 @@ const Nav: React.FC = () => {
                             >
                               <li>
                                 <a
-                                  href="#"
-                                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
-                                >
-                                  Dashboard
-                                </a>
-                              </li>
-                              <li>
-                                <a
-                                  href="#"
-                                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
-                                >
-                                  Settings
-                                </a>
-                              </li>
-                              <li>
-                                <a
-                                  href="#"
-                                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
-                                >
-                                  Earnings
-                                </a>
-                              </li>
-                              <li>
-                                <a
                                   onClick={() => {
                                     dispatch(loginUserDetails({}));
                                     dispatch(logout());

36fd1cfd140d020774e009b101e2caf005cd1236
sgaurav <sgaurav@acecamgolf.com>
2024-10-29 10:05:00 +0000

    Merged PR 559: AC-786 AC-787
    AC-786 AC-787

--- source_code/src/components/PlayingCart/CartItem.tsx
+++ source_code/src/components/PlayingCart/CartItem.tsx
@@ -4,7 +4,6 @@ import golfStickWithTee from "../../assets/images/image 8.png";
 import { useDispatch } from "react-redux";
 import { removeSelectedContest } from "../../reducers/Courses_data/courses";
 import moment from "moment";
-import { convertToLocalTime } from "../../utils/TimeUtils";
 
 interface ContestProps {
   contestId: number;
@@ -60,8 +59,6 @@ const CartItem: React.FC<{ contest: ContestProps }> = ({ contest }) => {
     );
     setIsRegistrationOpen(open);
   }, [currentTime, contest.registrationStartTime, contest.registrationEndTime]);
-
-  console.log("convertToLocalTime", convertToLocalTime);
 
   return (
     <div>

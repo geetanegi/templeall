5e7d8e2818d6d7e5c72f9be98abba6f1729909b0
sgaurav <sgaurav@acecamgolf.com>
2024-11-07 09:09:31 +0000

    Merged PR 660: copyright text and terms and privacy policy added
    copyright text and terms and privacy policy added

--- source_code/src/pages/Register.tsx
+++ source_code/src/pages/Register.tsx
@@ -29,8 +29,8 @@ import { API_URL } from "../services/enums";
 import dayjs from "dayjs";
 import { PasswordRegex } from "../utils/passwordValidation";
 import { ALPHANUMERIC_REGEX } from "../utils/RegexPatterns";
-import TermsAndConditionsPdf from "../assets/Pdf/AceCam Golf Terms and Conditions.pdf";
-import privacyPolicyPdf from "../assets/Pdf/AceCam Golf Privacy Policy.pdf";
+import TermsAndConditionsPdf from "../assets/Pdf/AceCam Golf Terms and Conditions.docx.pdf";
+import privacyPolicyPdf from "../assets/Pdf/AceCam Golf Privacy Policy.docx.pdf";
 
 import { downloadFile } from "../utils/downloadUtils";
 
@@ -550,6 +550,13 @@ const Register: React.FC = () => {
                 >
                   {" "}
                   Privacy Policy
+                </p>
+              </div>
+            </div>
+            <div className="fixed bottom-14 left-[70px] hidden h-0.5 w-[17%] items-end md:flex">
+              <div className="right-1 top-[1px] flex md:absolute">
+                <p className={`whitespace-nowrap p-2 text-[13px] text-white`}>
+                  © 2024 AceCam Golf, LLC. All rights reserved.
                 </p>
               </div>
             </div>

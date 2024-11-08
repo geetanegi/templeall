741618d71c8a297d20b92334edb01427868d13b6
Bhushan Bhandari <bhushanb@acecamgolf.com>
2024-11-08 12:13:59 +0000

    Merged PR 698: copy right changes
    copy right changes

--- source_code/src/components/AdminPanel/Adminpanel.tsx
+++ source_code/src/components/AdminPanel/Adminpanel.tsx
@@ -56,25 +56,10 @@ const Adminpanel: React.FC<AdminPanelProps> = ({ isCourseAdmin = false }) => {
   };
 
   let usersCount = [
-    {
-      roleIds: 3,
-      key: "Player User",
-      role: "Players",
-      icon: <Users size={16} />,
-    },
-    {
-      roleIds: 2,
-      key: "Course Admin",
-      role: "Course Admin",
-      icon: <LandPlot size={16} />,
-    },
-    {
-      roleIds: 1,
-      key: "Super Admin",
-      role: "Super Admin",
-      icon: <UserCog size={16} />,
-    },
-  ];
+    { roleIds: 3, key: "Player User", role: "Players", icon: <Users size={16} /> },
+    { roleIds: 2, key: "Course Admin", role: "Course Admin", icon: <LandPlot size={16} /> },
+    { roleIds: 1, key: "Super Admin", role: "Super Admin", icon: <UserCog size={16} /> }
+]
   if (isCourseAdmin) {
     usersCount = [];
     usersCount = [
@@ -86,6 +71,8 @@ const Adminpanel: React.FC<AdminPanelProps> = ({ isCourseAdmin = false }) => {
       },
     ];
   }
+
+ 
 
   return (
     <PageLoader isActive={loader}>

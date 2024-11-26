9bfc04b73cf8024a5ba9f345c1f4537b31126aa9
SPriya <spriya@netlink.com>
2024-11-26 16:20:32 +0530

    Ui fixes

--- source_code/src/components/TableComponent.tsx
+++ source_code/src/components/TableComponent.tsx
@@ -14,6 +14,7 @@ interface TableComponentProps {
   style?: any;
   oddRowStyle?: React.CSSProperties;
   evenRowStyle?: React.CSSProperties;
+  thirdRowStyle?: React.CSSProperties;
   greenTheme?: boolean;
   totalElement?: number;
   elementPerPage?: number
@@ -29,6 +30,7 @@ const TableComponent: React.FC<TableComponentProps> = ({
   style = {},
   oddRowStyle = {},
   evenRowStyle = {},
+  thirdRowStyle = {},
   greenTheme = false,
   totalElement = 10,
   elementPerPage = 10
@@ -41,7 +43,7 @@ const TableComponent: React.FC<TableComponentProps> = ({
   };
 
   return (
-    <div className="mb-2 mt-3 flex h-full text-sm mb-5">
+    <div className="mb-2 mt-3 flex h-full text-sm mb-1">
       <div className="w-full">
         <div
           className={`w-full overflow-x-scroll ${rowData.length ? "rounded-lg" : "rounded-t-lg"} 2xl:max-w-none mt-2 border border-gray-100 md:overflow-auto`}
@@ -90,13 +92,14 @@ const TableComponent: React.FC<TableComponentProps> = ({
               {rowData?.map((data, index) => (
                 <tr
                   key={index}
-                  style={(index + 1) % 2 !== 0 ? oddRowStyle : evenRowStyle}
+                  style={(index + 1) % 3 === 0 ? thirdRowStyle : (index + 1) % 3 === 1 ? oddRowStyle : evenRowStyle}
                 >
                   {Object.entries(data).map(([key], index) => {
                     return (
                       <td
                         key={index}
                         className={`whitespace-nowrap px-3 py-2 font-normal text-black`}
+                        style={(index + 1) % 3 === 0 ? { color: '#fff' } : { color: '#000' }}
                       >
                         {data[key]}
                       </td>
@@ -115,19 +118,6 @@ const TableComponent: React.FC<TableComponentProps> = ({
         {pagination ? (
           <div className="mt-1.5 mt-5 flex w-full flex-col items-center justify-center gap-5 px-1 sm:flex-row sm:justify-between">
             <div className="flex items-center justify-center">
-              {/* <div>Rows</div>
-              <select
-                name="example"
-                id="example"
-                onChange={handlePageSizeChange}
-                className="mx-2 h-[30px] rounded-md border border-gray-200 px-5"
-              >
-                {rowCount.map((row) => (
-                  <option selected={pageSize === row} value={row}>
-                    {row}
-                  </option>
-                ))}
-              </select> */}
               <div>Showing results {currentPage * 10 + 1} to {currentPage * 10 + elementPerPage} of {totalElement}</div>
             </div>
             <PaginationComponent

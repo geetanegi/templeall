eb277eea9554f896f5c89821afd2d84e4095e46b
Somya Priya <spriya@acecamgolf.com>
2024-11-26 05:38:35 +0000

    Merged PR 893: fix ui & pdf

--- source_code/vite.config.ts
+++ source_code/vite.config.ts
@@ -1,15 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
- 
-// https://vitejs.dev/config/
-// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
 export default defineConfig({
-  base: '/',  // Replace with your desired base path
+  base: "/", // Replace with your desired base path
   plugins: [react()],
   server: {
     port: 8080, // Replace 3001 with your desired port
   },
   define: {
     global: {},
-},
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          // Remove hash for PDF files
+          if (assetInfo.name && assetInfo.name.endsWith(".pdf")) {
+            return "assets/[name].[ext]";
+          }
+          // Keep hash for other assets
+          return "assets/[name]-[hash].[ext]";
+        },
+      },
+    },
+  },
 });

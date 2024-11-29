a30ea998e4974684df185bc986c39b2763dfed68
sgaurav <sgaurav@netlink.com>
2024-11-29 14:53:51 +0530

    social login screen implemented

--- source_code/src/pages/AuthStructure.tsx
+++ source_code/src/pages/AuthStructure.tsx
@@ -9,10 +9,12 @@ import { ROUTES } from "../utils/routesPath";
 import PageLoader from "../components/PageLoader";
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
+import AceCamUI from "../components/AceCamUI";
 
 import { viewPdf } from "../utils/downloadUtils";
 import privacyPolicyPdf from "../assets/Pdf/AceCamGolfPrivacyPolicy.pdf";
 import TermsAndConditionsPdf from "../assets/Pdf/AceCamGolfTermsandConditions.pdf";
+import SocialLoginScreen from "../components/social-login/SocialLoginScreen";
 
 const AuthStructure: React.FC = () => {
   const location = useLocation();
@@ -37,12 +39,12 @@ const AuthStructure: React.FC = () => {
             className={`align-center flex h-screen w-full flex-col bg-backgroundDark ${location.pathname === ROUTES.SIGNUP ? "" : "justify-center"} overflow-y-auto`}
           >
             {location.pathname === ROUTES.LOGIN && (
-              <div className="mx-auto flex w-full flex-col items-center justify-center md:w-[260px]">
+              <div className="mx-auto flex w-full flex-col items-center justify-center px-5 py-12 md:w-[590px]">
                 {location.pathname === ROUTES.LOGIN && <Login />}
               </div>
             )}
             {location.pathname === ROUTES.FORGET_PASSWORD && (
-              <div className="mx-auto flex w-full flex-col items-center justify-center py-12 md:w-[260px]">
+              <div className="mx-auto flex w-full flex-col items-center justify-center px-5 py-12 md:w-[590px]">
                 {location.pathname === ROUTES.FORGET_PASSWORD && (
                   <ForgetPassword />
                 )}
@@ -56,7 +58,7 @@ const AuthStructure: React.FC = () => {
               </div>
             )}
             {location.pathname === ROUTES.SIGNUP && (
-              <div className="mx-auto my-auto flex h-max-content w-full flex-col items-center justify-center py-4 md:w-[260px]">
+              <div className="mx-auto my-auto flex h-max-content w-full flex-col items-center justify-center px-5 py-12 md:w-[590px]">
                 {location.pathname === ROUTES.SIGNUP && (
                   // <StripeIntegration>
                   <Register />
@@ -66,40 +68,44 @@ const AuthStructure: React.FC = () => {
             )}
 
             {location.pathname === ROUTES.STRIPE && (
-              <div className="flex w-full flex-col items-center justify-center p-6 pt-[100px] md:w-3/5 md:p-8">
-                {/* <StripeIntegration></StripeIntegration> */}
+              <div className="mx-auto my-auto flex h-max-content w-full flex-col items-center justify-center px-5 py-12 md:w-[590px]">
+                {location.pathname === ROUTES.STRIPE && (
+                  // <StripeIntegration>
+                  <SocialLoginScreen />
+                  // </StripeIntegration>
+                )}
               </div>
             )}
 
-            <div className="relative mt-[20px]">
-              <div className="flex items-center justify-center gap-3">
-                <p
-                  onClick={downloadTermsAndConditionsFunc}
-                  className={`cursor-pointer whitespace-nowrap text-[13px] text-link hover:underline`}
-                >
-                  Terms and Conditions
-                </p>{" "}
-                <p className="cursor-pointer whitespace-nowrap text-[13px] text-[#FFFFFF] hover:underline">
-                  |
-                </p>{" "}
-                <p
-                  className={`cursor-pointer whitespace-nowrap text-[13px] text-link hover:underline`}
-                  onClick={downloadPrivacyPolicyFunc}
-                >
-                  {" "}
-                  Privacy Policy
-                </p>
-                <p className="cursor-pointer whitespace-nowrap text-[13px] text-[#FFFFFF] hover:underline">
-                  |
-                </p>{" "}
-                <p
-                  className={`cursor-pointer whitespace-nowrap text-[13px] text-link hover:underline`}
-                >
-                  <a href="mailto:support@acecamgolf.com">Contact Us</a>
-                </p>
-              </div>
-              <div className="flex items-center justify-center">
-                <p className={`whitespace-nowrap text-[13px] text-white`}>
+            <div className="-mt-10 flex items-center justify-center gap-2 md:hidden">
+              <p
+                onClick={downloadTermsAndConditionsFunc}
+                className={`cursor-pointer whitespace-nowrap text-[13px] text-link hover:underline`}
+              >
+                Terms and Conditions
+              </p>{" "}
+              <p className="cursor-pointer whitespace-nowrap text-[13px] text-[#FFFFFF] hover:underline">
+                |
+              </p>{" "}
+              <p
+                className={`cursor-pointer whitespace-nowrap text-[13px] text-link hover:underline`}
+                onClick={downloadPrivacyPolicyFunc}
+              >
+                {" "}
+                Privacy Policy
+              </p>
+              <p className="cursor-pointer whitespace-nowrap text-[13px] text-[#FFFFFF] hover:underline">
+                |
+              </p>{" "}
+              <p
+                className={`cursor-pointer whitespace-nowrap text-[13px] text-link hover:underline`}
+              >
+                <a href="mailto:support@acecamgolf.com">Contact Us</a>
+              </p>
+            </div>
+            <div className="-mt-0 mb-10 flex items-center justify-center gap-2 md:hidden">
+              <div className="right-1 top-[1px] flex md:absolute">
+                <p className={`whitespace-nowrap p-2 text-[13px] text-white`}>
                   © 2024 AceCam
                   <sup className="text-[8px]">TM&nbsp;</sup>{" "}
                   {/* <span className="align-super text-xs">™&nbsp;</span> */}

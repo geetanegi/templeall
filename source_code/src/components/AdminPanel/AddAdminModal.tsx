d88a6e9a492558c222e58367f559a611ab0965da
BHUSHANB1 <bhushanb@netlink.com>
2024-12-06 17:09:14 +0530

    contest bug fixes

--- source_code/src/components/AdminPanel/AddAdminModal.tsx
+++ source_code/src/components/AdminPanel/AddAdminModal.tsx
@@ -203,13 +203,12 @@ const AddAdminModal: React.FC<AddAdminModalProps> = ({
                     onChange={handleChange}
                     onBlur={handleBlur}
                     maxLength={100}
-                    className={`rounded-lg border bg-gray-100 px-2 py-3 text-gray-500 ${
-                      touched.firstName &&
+                    className={`rounded-lg border bg-gray-100 px-2 py-3 text-gray-500 ${touched.firstName &&
                       errors.firstName &&
                       typeof errors.firstName === "string"
-                        ? "border-red-500"
-                        : "border-gray-200"
-                    } `}
+                      ? "border-red-500"
+                      : "border-gray-200"
+                      } `}
                   />
                   <span
                     className={`pointer-events-none absolute left-[45%] top-3 text-red-500 ${values.firstName ? "hidden" : ""}`}
@@ -237,13 +236,12 @@ const AddAdminModal: React.FC<AddAdminModalProps> = ({
                     onChange={handleChange}
                     onBlur={handleBlur}
                     maxLength={100}
-                    className={`rounded-lg border bg-gray-100 px-2 py-3 text-gray-500 ${
-                      touched.lastName &&
+                    className={`rounded-lg border bg-gray-100 px-2 py-3 text-gray-500 ${touched.lastName &&
                       errors.lastName &&
                       typeof errors.lastName === "string"
-                        ? "border-red-500"
-                        : "border-gray-200"
-                    } `}
+                      ? "border-red-500"
+                      : "border-gray-200"
+                      } `}
                   />
                   <span
                     className={`pointer-events-none absolute left-[45%] top-3 text-red-500 ${values.lastName ? "hidden" : ""}`}

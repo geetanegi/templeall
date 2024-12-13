c11b656635992a9c937c3e2d23476951e17ba4c4
BHUSHANB1 <bhushanb@netlink.com>
2024-12-13 11:36:35 +0530

    .

--- source_code/src/components/PlayingCart/PlayingCart.tsx
+++ source_code/src/components/PlayingCart/PlayingCart.tsx
@@ -40,7 +40,7 @@ const PlayingCart: React.FC = () => {
         <div className="rounded-lg bg-gray-100">
           <div className="mb-4 flex justify-between p-4">
             <span className="text-gray-700">Contest</span>
-            <span className="text-gray-700">Total Prize</span>
+            <span className="text-gray-700">Total Price</span>
             <span></span>
           </div>
         </div>

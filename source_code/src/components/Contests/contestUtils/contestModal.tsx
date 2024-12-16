c48395aba5f44121ae075a292e85869743bcb823
BHUSHANB1 <bhushanb@netlink.com>
2024-12-16 19:03:45 +0530

    .

---
+++ source_code/src/components/Contests/contestUtils/contestModal.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Modal from "../../ModalComponent";
+import Contests from "../../../pages/Contests";
+
+interface contestModalProps {
+  isContestModalOpen: boolean;
+  setIsContestModalOpn: (flag: boolean) => void;
+  contestId: number | string;
+  setContestId: (value: any) => void;
+}
+
+const ContestModal: React.FC<contestModalProps> = ({
+  isContestModalOpen,
+  setIsContestModalOpn,
+  contestId,
+  setContestId,
+}) => {
+
+const handleClose = () => {
+    setContestId("");
+    setIsContestModalOpn(false);
+  }
+  return (
+    <Modal
+      isOpen={isContestModalOpen}
+      onClose={handleClose}
+      title="View Contest"
+    >
+      <div className="h-[80vh] p-5  rounded-md overflow-auto">
+        <Contests contestId={contestId} handleClose={handleClose} />
+      </div>
+    </Modal>
+  );
+};
+
+export default ContestModal;

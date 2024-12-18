f0631147fa9bd039edc4e04f046726b7f9aaabb6
BHUSHANB1 <bhushanb@netlink.com>
2024-12-18 21:23:41 +0530

    .

--- source_code/src/components/Contests/ContestViewScreen.tsx
+++ source_code/src/components/Contests/ContestViewScreen.tsx
@@ -203,9 +203,7 @@ const ContestViewScreen: React.FC<ContestViewScreenProps> = ({
       <div className="mb-7 items-center gap-4">
         <span className="text-[11px] font-bold">Eligibility Requirements</span>
         <p className="text-[14px]">
-          {contestData?.note || ""} Your HDCP must be no less than 7.0 to be
-          eligible for participation, unless you are woman, or older than 70
-          years of age. Your HDCP must be no less t
+          {contestData?.note || ""}
         </p>
       </div>
       <div className="mb-2 flex items-center">

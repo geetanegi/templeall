72f7aa5f9bbbcd62d57943f22299bef28d12c803
Bhushan Bhandari <BhushanB@bpl.netlink.com>
2024-12-31 10:45:53 +0530

    .

--- source_code/src/components/AdminPanel/AddpanalUtils/AddPanelUtils.tsx
+++ source_code/src/components/AdminPanel/AddpanalUtils/AddPanelUtils.tsx
@@ -3,15 +3,15 @@
 export const computeTableHeaders = (userType: string) =>{
     if (userType === 'player') {
          return [
-            { id: 1, field: "Name" },
-            { id: 2, key: "userName", field: "Username" },
+            { id: 1, key: "firstName", field: "Name" },
+            { id: 2, key: "username", field: "Username" },
             { id: 3, key: "email", field: "Email Address" },
             { id: 4, key: "status", field: "Status" },
         ]
     } else if (userType === 'courseAdmin' || userType === 'superAdmin') {
         return [
-            { id: 1, field: "Name" },
-            { id: 2, key: "userName", field: "Username" },
+            { id: 1, key:"firstName", field: "Name" },
+            { id: 2, key: "username", field: "Username" },
             { id: 3, key: "email", field: "Email Address" },
             { id: 4, key: "Action", field: "Action" },
         ]

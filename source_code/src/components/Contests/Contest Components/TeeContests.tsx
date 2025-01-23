4b9a1b5a90081e4cb482531ef965a94df9f7b108
Bhushan Bhandari <BhushanB@bpl.netlink.com>
2025-01-23 16:55:19 +0530

    .

--- source_code/src/components/Contests/Contest Components/TeeContests.tsx
+++ source_code/src/components/Contests/Contest Components/TeeContests.tsx
@@ -186,7 +186,7 @@ const TeeContests: React.FC<{ teeContest: TeeContest }> = ({ teeContest }) => {
               <img src={golfStickWithTee} alt="" className="h-14 w-14" />
               <div className="pl-1">
                 <p className="text-sm font-semibold">
-                  {teeContest?.contestType + " " + teeContest?.cid}
+                {`${teeContest?.contestType} (${teeContest?.cid})`}
                 </p>
                 <p className="text-sm font-semibold text-red-600">
                   ${teeContest.entryFee}
